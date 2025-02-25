7dbe1c6653bb57236386b5b66ee6e31f21f2aac3
Suk Jin Hong <sh1949@ilab1.cs.rutgers.edu>
2025-02-24 20:02:21 -0500

    Second Edit

--- P1/memgrind.c
+++ P1/memgrind.c
@@ -15,10 +15,106 @@ void stress_test1() {
 // Test 2: 120개의 1바이트를 할당한 후에, 한번에 free()를 실행
 void stress_test2() {
     void *ptr_lst[120];
+
     for (int i = 0; i < 120; i++) {
+        printf("%d\n", i);
         ptr_lst[i] = malloc(1);
     }
     for (int i = 0; i < 120; i++) {
         free(ptr_lst[i]);
     }
+}
+
+// Test 3: 
+void stress_test3() {
+    void *arr[120];
+    for(int i = 0; i < 120; i++){
+        int whatDo = rand();
+        int arrIndex = 0;
+        if(whatDo >= RAND_MAX / 2){ 
+            while(arrIndex < 120){
+                if(arr[arrIndex] == NULL){
+                    arr[arrIndex] = malloc(1);
+                    arrIndex = 0;
+                    break;
+                }
+                arrIndex++;
+            }
+        } else {
+            while(arrIndex < 120){
+                if(*(int *)(arr[arrIndex] - 4) == 1){
+                    free(arr[arrIndex]);
+                    arrIndex = 0;
+                    break;
+                }
+                arrIndex++;
+            }
+        }
+    }
+    for (int i = 0; i < 120; i++) {
+        if (*(int *)(arr[i] - 4) == 1) {
+            free(arr[i]);
+        }
+    }
+}
+
+// Test 4:
+void stress_test4() {
+    void* arr[120];
+    for(int i = 0; i < 120; i++){
+        int whatDo = rand();
+        int arrIndex = 0;
+        if(whatDo >= RAND_MAX / 2){ 
+            while(arrIndex < 120){
+                if(arr[arrIndex] == NULL){
+                    arr[arrIndex] = malloc((rand() % 512)*16);
+                    arrIndex = 0;
+                    break;
+                }
+                arrIndex++;
+            }
+        } else {
+            while(arrIndex < 120){
+                if(*(int *)(arr[arrIndex] - 4) == 1){
+                    free(arr[arrIndex]);
+                    arrIndex = 0;
+                    break;
+                }
+                arrIndex++;
+            }
+        }
+    }
+    for (int i = 0; i < 120; i++) {
+        if (*(int *)(arr[i] - 4) == 1) {
+            free(arr[i]);
+        }
+    }
+}
+// Test 5: 
+void stress_test5() {
+    void *arr[100];
+    for (int i = 0; i < 100; i++) {
+        arr[i] = malloc(rand() % 17 + 2);
+    }
+    for (int i = 0; i < 100; i+=2) {
+        free(arr[i]);
+    }
+    for (int i = 1; i < 100; i+=2) {
+        free(arr[i]);
+    }
+}
+
+// Test 6
+void leak_test() {
+    malloc(1);
+}
+
+
+int main() {
+    //stress_test2();
+    //stress_test3();
+    stress_test4();
+    //stress_test5();
+    //leak_test();
+    return 0;
 }
--- P1/mymalloc.c
+++ P1/mymalloc.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 
-#include <mymalloc.h>
+#include "mymalloc.h"
 #define MEMLENGTH 4096  // heap memory size - 4096 bytes
 int isIni = 0;  // check that the heap is initialized
 
@@ -12,11 +12,29 @@ static union {
     double align;
 } heap;
 
+void leak_detector(){
+    //int num_bytes = 0;
+    int num_objects = 0;
+    int location = 0;
+    while(location < MEMLENGTH - 8){
+        if(*(int *)(heap.bytes + location + 4) == 1){
+            //num_bytes += *(int *)(heap.bytes + location);
+            num_objects++;
+        }
+        location += *(int *)(heap.bytes + location) + 8;
+    }
+    if(num_objects > 0){
+        //printf("mymalloc: %d bytes leaked in %d objects", num_bytes, num_objects);
+        printf("mymalloc: %d objects leaked", num_objects);
+    }
+}
+
 void init_heap() {
     if (!isIni) {
         *(int *)(heap.bytes) = 4088;
         *(int *)(heap.bytes + 4) = 0;
         isIni = 1;
+        atexit(leak_detector);
     }
 }
 
@@ -28,7 +46,7 @@ void *mymalloc(size_t size, char *file, int line) {
     int location = 0;   // start from the beginning of the heap
 
     while(location <= MEMLENGTH - 8) {
-        int chunkSize = *(int *)(heap.bytes + location);    // the current chunk size
+        size_t chunkSize = *(size_t *)(heap.bytes + location);    // the current chunk size
         int chunkAllocation = *(int *)(heap.bytes + location + 4);  // check the allocation status
         if (chunkSize >= size && chunkAllocation == 0) {    // if the current block is large enough and unallocated 
             if (chunkSize == size) {    //  if the block size exactly matches
@@ -38,11 +56,11 @@ void *mymalloc(size_t size, char *file, int line) {
                 *(int *)(heap.bytes + location) = size;     // set the size of the first split block
                 *(int *)(heap.bytes + location + 4) = 1;    // set the first block as allocated
 
-                *(int *)(heap.bytes + location + size) = chunkSize - size;  // set the size of the new block
-                *(int *)(heap.bytes + location + size + 4) = 0; // set the new block as unallocated
+                *(int *)(heap.bytes + location + size + 8) = chunkSize - size;  // set the size of the new block
+                *(int *)(heap.bytes + location + size + 12) = 0; // set the new block as unallocated
                 return (void *)(heap.bytes + location + 8); // return the start address
             }
-        } else {
+        } else { 
         location += chunkSize + 8;  // move to next block
         }
     }
@@ -54,7 +72,7 @@ void badPointer(void *ptr, char *file, int line) {
     // Condition 1
     int diff = (char *)ptr - (char *)heap.bytes;    // ptr가 heap.bytes 내부에 있는지 확인 (내부에 있으면 ptr가 heap.bytes보다 큼)
     if (diff < 0 || diff >= MEMLENGTH) {
-        fprintf("free: Inappropriate pointer (%s.c%d)", file, line);
+        fprintf(stderr, "free: Inappropriate pointer (%s.c%d)", file, line);
         exit(2);
     } 
     // Condition 2
@@ -68,12 +86,12 @@ void badPointer(void *ptr, char *file, int line) {
         location += *(int *)(heap.bytes + location) + 8;
     }
     if (!found) {
-        fprintf("free: Inappropriate pointer (%s.c%d)", file, line);
+        fprintf(stderr, "free: Inappropriate pointer (%s.c%d)", file, line);
         exit(2);
     }
     // Condition 3
     if (*(int *)(ptr - 4) == 0) {
-        fprintf("free: Inappropriate pointer (%s.c%d)", file, line);
+        fprintf(stderr, "free: Inappropriate pointer (%s.c%d)", file, line);
         exit(2);
         // similiar with condition 2
     }
@@ -102,7 +120,6 @@ void coalesce() {
 
 void myfree(void *ptr, char *file, int line) {
     badPointer(ptr, file, line); 
-    int *isAlloc = *(int *)(ptr - 4);
-    *isAlloc = 0;
+    *(int *)(ptr - 4) = 0;
     coalesce();
 }
